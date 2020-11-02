b8da92e1feb5af9348d0082b47466d0a99eabd44
John <64992493+JohnDanielScanlon@users.noreply.github.com>
2020-11-02 04:38:21 -0600

    Update OpenboxMX.sh

--- Bash/OpenboxMX.sh
+++ Bash/OpenboxMX.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
-#Testing --- bash -n install.sh
 
 # Main Dots - https://github.com/owl4ce/dotfiles.git
 #Goddammit so in the future if you're running into issues with MX/antiX and repos/ debian packages failing. You simply need to install software-properties-common checkaptgpg 
 
 #This script sucks and keeps breaking. Use on a new install, don't 1v1 your old install with a random shell script
 #Deps for pretty much everything
-sudo apt update && sudo apt install meson gcc software-properties-common checkaptgpg openbox obconf alsa-utils brightnessctl nitrogen dunst tint2 gsimplecal rofi lxappearance qt5ct qt5-style-plugins lxpolkit xautolock rxvt-unicode xclip scrot thunar thunar-archive-plugin thunar-media-tags-plugin thunar-volman ffmpegthumbnailer tumbler w3m w3m-img geany nano vim viewnior mpd mpc ncmpcpp mpv pavucontrol parcellite neofetch htop zsh imagemagick ffmpeg playerctl libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev  libpcre2-dev  libevdev-dev uthash-dev libev-dev libx11-xcb-dev libpcre3-dev slim xorg obmenu 
-echo 'you should do a snapshot now because here come the GITS BABY'
-sleep 10
-sudo apt update
+sudo apt update && sudo apt install meson gcc software-properties-common openbox obconf alsa-utils brightnessctl nitrogen dunst tint2 gsimplecal rofi lxappearance qt5ct qt5-style-plugins lxpolkit xautolock rxvt-unicode xclip scrot thunar thunar-archive-plugin thunar-media-tags-plugin thunar-volman ffmpegthumbnailer tumbler w3m w3m-img geany nano vim viewnior mpd mpc ncmpcpp mpv pavucontrol parcellite neofetch htop zsh imagemagick ffmpeg playerctl libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev  libpcre2-dev  libevdev-dev uthash-dev libev-dev libx11-xcb-dev libpcre3-dev slim xorg obmenu 
+
+#checkaptgpg-antix
 
 #You may have issues here. Try running checkaptgpg/ sudo mx-repo-manager 
 echo 'deb http://download.opensuse.org/repositories/home:/Head_on_a_Stick:/obmenu-generator/Debian_10/ /' | sudo tee /etc/apt/sources.list.d/home:Head_on_a_Stick:obmenu-generator.list
 curl -fsSL https://download.opensuse.org/repositories/home:Head_on_a_Stick:obmenu-generator/Debian_10/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home:Head_on_a_Stick:obmenu-generator.gpg > /dev/null
 
-sudo checkaptgpg
 sudo apt update && sudo apt install obmenu-generator
 
 #Picom 
-
 sudo apt install compton
 
 #sudo apt install libgtk2-perl libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev  libpcre2-dev  libevdev-dev uthash-dev libev-dev libx11-xcb-dev
@@ -30,7 +26,6 @@ sudo apt install compton
 #meson --buildtype=release . build
 #ninja -C build
 #sudo ninja -C build install
-
 #rm -r /tmp/picom
 
 cd ~/ 
@@ -48,7 +43,7 @@ fc-cache -rv
 echo 'zsh install coming, if you're kinky like that. Did you know that you can kill PIDs with tab completion in zsh? Like say you wanted to kill compton, you can type kill c(hit tab) and the PIDs starting with c will autocomplete. Is there an alternative in bash? idk, install is starting now. Anyway, do you want to install it?'
 
 while true; do
-    read -p "zsh install coming, if you're kinky like that. Did you know that you can kill PIDs with tab completion in zsh? Like say you wanted to kill compton, you can type kill c(hit tab) and the PIDs starting with c will autocomplete. Is there an alternative in bash? idk, install is starting now. Anyway, do you want to install it+ ohmyzsh?" yn
+    read -p "zsh install coming, if you're kinky like that. yn
     case $yn in
         [Yy]* ) 
       chsh -s /usr/bin/zsh # Change default shell to zsh for current user
@@ -60,5 +55,4 @@ while true; do
     esac
 done
 
-
-EOF
+sudo vim ~/.config/openbox/autostart #replace picom with compton
