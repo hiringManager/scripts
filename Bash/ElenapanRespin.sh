e4f5726eb9b66f54b898d85c856440536a955115
onwhAt <64992493+scanlon-dev@users.noreply.github.com>
2020-11-28 09:02:22 -0600

    Update ElenapanRespin.sh

--- Bash/ElenapanRespin.sh
+++ Bash/ElenapanRespin.sh
@@ -4,6 +4,9 @@
 
 #Install MX or Debian something
 
+#Dependencies
+sudo apt install curl rofi kitty thunar neovim 
+
 #Download + Configure pywal and append zshrc+bashrc
 
 sudo apt install python3 python3-pip python3-setuptools python3-wheel
@@ -13,6 +16,9 @@ pip3 install pywal
 
 git clone https://github.com/elenapan/dotfiles
 
+#Install ohmyzsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 #Pull Colortoys and combine with elenascripts? Maybe too much bloat. Is there a way to make it be called with color(toolname)?
 git clone https://github.com/owl4ce/dotfiles/tree/master/.color-toys
 mv ./.color-toys ~/.colortoys
@@ -20,6 +26,11 @@ mv ./.color-toys ~/.colortoys
 #Is doom going to persist?
 #Is Zsh going to persist? If not, where in the snapshot dir removal is it getting fucked up?
 
+#Does this work with these dots? 
+
+git clone https://github.com/scanlon-dev/Scripts/edit/main/Bash/RofiPywal.sh
+
+./RofiPywal.sh
 
 
 
