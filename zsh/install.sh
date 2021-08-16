9f290a45bc16e6b35d682f6f2fcee1638def7471
JD <64992493+hiringManager@users.noreply.github.com>
2021-08-15 23:55:59 -0500

    Update install.sh

--- zsh/install.sh
+++ zsh/install.sh
@@ -3,13 +3,8 @@
 # Antigen
 mkdir ~/.zsh
 curl -L git.io/antigen > ~/.zsh/antigen.zsh
-## Shouldn't there be a git for this? and why does curl no mkdir
-
+cp ./.zshrc ~/.zshrc
 # Vundle
 # https://github.com/VundleVim/Vundle.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-# Move Configs
-mv ~/.zshrc ~/.zshbak 
-mv ~/.vimrc ~/.vimbak
-mv .zshrc .vimrc ~/
-zsh
+cp ./.vimrc ~/.vimrc
