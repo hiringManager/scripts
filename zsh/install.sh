81f55608fbb3acd7957bb48b50a57bd7880bf09c
JD <64992493+hiringManager@users.noreply.github.com>
2021-09-07 18:06:33 -0500

    Update install.sh

--- zsh/install.sh
+++ zsh/install.sh
@@ -6,5 +6,5 @@ curl -L git.io/antigen > ~/.zsh/antigen.zsh
 cp ./.zshrc ~/.zshrc
 # Vundle
 # https://github.com/VundleVim/Vundle.vim
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cp ./.vimrc ~/.vimrc
+# git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# cp ./.vimrc ~/.vimrc
